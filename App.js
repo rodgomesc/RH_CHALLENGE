d9b6fba1c43143cfded6f4bc34c808b65630a4ac
Rodrigo <rodgomesc@gmail.com>
2019-09-11 10:09:10 -0300

    ESLint, Prettier & EditorConfig

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ import {
 
 const App = () => {
   return (
-    <Fragment>
+    <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
@@ -68,7 +68,7 @@ const App = () => {
           </View>
         </ScrollView>
       </SafeAreaView>
-    </Fragment>
+    </>
   );
 };
 
