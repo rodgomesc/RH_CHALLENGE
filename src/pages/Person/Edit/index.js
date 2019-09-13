93b5d50f1c19161dc3a0bad288165e0cbdbd6f40
Rodrigo <rodgomesc@gmail.com>
2019-09-12 21:03:11 -0300

    feat: Edit page

--- src/pages/Person/Edit/index.js
+++ src/pages/Person/Edit/index.js
@@ -1,8 +1,61 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Container, Wrapper, RowWrapper, ButtonWrapper} from './styles';
 
-// import { Container } from './styles';
+import LinearExtraHeader from '../../../components/LinearExtraHeader';
+import LinearButton from '../../../components/LinearButton';
+import Input from '../../../components/Input';
+import DatePicker from '../../../components/DatePicker';
+import Picker from '../../../components/Picker';
 
-export default function PersonEdit() {
-  return <View />;
+export default function PersonEdit({navigation}) {
+  function handleClick() {
+    navigation.navigate('PersonList');
+  }
+  return (
+    <>
+      <Container>
+        <LinearExtraHeader />
+      </Container>
+      <Wrapper>
+        <RowWrapper>
+          <Input title="Nome" icon="face" />
+        </RowWrapper>
+
+        <RowWrapper>
+          <Input title="Genero" icon="wc" />
+          <DatePicker placeholder="Nascimento" />
+        </RowWrapper>
+
+        <RowWrapper>
+          <Input title="Município de nascimento" icon="call" />
+          <Input title="Matrícula" icon="call" />
+        </RowWrapper>
+
+        <RowWrapper>
+          <Picker icon="pan-tool">
+            <Picker.Item label="Solteiro" value="M" />
+            <Picker.Item label="Casado" value="F" />
+            <Picker.Item label="Divorciado" value="F" />
+          </Picker>
+          <Picker icon="swap-calls">
+            <Picker.Item label="Masculino" value="M" />
+            <Picker.Item label="Feminino" value="F" />
+          </Picker>
+        </RowWrapper>
+
+        <RowWrapper>
+          <Input title="Cargo" icon="work" />
+          <Input title="Lotação" icon="directions-bus" />
+        </RowWrapper>
+
+        <ButtonWrapper>
+          <LinearButton title="SALVAR" onPress={handleClick} />
+        </ButtonWrapper>
+      </Wrapper>
+    </>
+  );
 }
+
+PersonEdit.navigationOptions = {
+  title: 'Editar Pessoa',
+};
---
+++ src/pages/Person/Edit/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components/native';
+import {Card} from 'native-base';
+
+export const Container = styled.View`
+  flex: 1;
+  background: #eee;
+`;
+
+export const Wrapper = styled(Card)`
+  position: absolute;
+  flex-direction: column;
+  background: #fff;
+  margin: 5px 15px 0;
+  padding: 10px 10px 0;
+  border-radius: 4px;
+  height: 380;
+  width: 380;
+`;
+
+export const ButtonWrapper = styled.View`
+  margin-top: 10;
+`;
+
+export const RowWrapper = styled.View`
+  flex-direction: row;
+`;
