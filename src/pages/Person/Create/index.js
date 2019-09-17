0473bd0c9c96363194cdfb018296719f85ad50aa
Rodrigo <rodgomesc@gmail.com>
2019-09-16 21:38:02 -0300

    - Layout Customization - implemment Validations - lot of fixes

--- src/pages/Person/Create/index.js
+++ src/pages/Person/Create/index.js
@@ -1,61 +1,207 @@
-import React from 'react';
-import {Container, Wrapper, RowWrapper, ButtonWrapper} from './styles';
+import React, {useEffect, useState} from 'react';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import {ToastAndroid as toast, Alert, KeyboardAvoidingView} from 'react-native';
+import fieldsValidate from '../../../utils/validation';
+import {Container, Wrapper, RowWrapper, Error, ButtonContainer} from './styles';
 
 import LinearExtraHeader from '../../../components/LinearExtraHeader';
 import LinearButton from '../../../components/LinearButton';
 import Input from '../../../components/Input';
 import DatePicker from '../../../components/DatePicker';
 import Picker from '../../../components/Picker';
+import database from '../../../config/firebase';
+import {Separator} from '../Detail/styles';
 
-export default function PersonEdit({navigation}) {
-  function handleClick() {
-    navigation.navigate('PersonList');
+export default function PersonCreate({navigation}) {
+  const [resultData, setResultData] = useState([]);
+
+  useEffect(() => {
+    async function fetchFromDb() {
+      // map each item to array
+      database.ref('person').on('value', snapshot => {
+        const items = [];
+        snapshot.forEach(child => {
+          items.push(child.val());
+        });
+        setResultData(items);
+      });
+    }
+
+    fetchFromDb();
+  }, []);
+
+  const schema = Yup.object().shape({
+    nome: Yup.string().required('Campo nome é obrigatório!'),
+    matricula: Yup.string().required('Campo Matricula é obrigatório!'),
+    lotacao: Yup.string().required('Campo Lotação é obrigatório!'),
+    cargo: Yup.string().required('Campo Cargo é obrigatório!'),
+    celular: Yup.string().required('Campo Celular é obrigatório!'),
+    telefone: Yup.string().required('Campo telefone é obrigatório!'),
+    email: Yup.string().required('Campo email é obrigatório!'),
+  });
+
+  async function handleSubmit(values) {
+    const ValidationMessages = fieldsValidate(resultData, values);
+
+    if (ValidationMessages) {
+      console.tron.log(ValidationMessages);
+      ValidationMessages.forEach(message => {
+        Alert.alert('AVISO', message);
+      });
+    } else {
+      await database.ref(`person/${values.matricula}`).set({
+        nome: values.nome,
+        nascimento: values.nascimento,
+        matricula: values.matricula,
+        sexo: values.sexo,
+        municipioNascimento: values.municipioNascimento,
+        estadoNascimento: values.estadoNascimento,
+        estadoCivil: values.estadoCivil,
+        lotacao: values.lotacao,
+        cargo: values.cargo,
+        celular: values.celular,
+        telefone: values.telefone,
+        email: values.email,
+      });
+
+      toast.show('Dados salvos com sucesso', toast.LONG);
+      navigation.navigate('PersonList');
+    }
   }
+
   return (
     <>
       <Container>
         <LinearExtraHeader />
       </Container>
-      <Wrapper>
-        <RowWrapper>
-          <Input title="Nome" icon="face" />
-        </RowWrapper>
+      <Formik
+        validationSchema={schema}
+        onSubmit={values => handleSubmit(values)}>
+        {props => (
+          <Wrapper>
+            <KeyboardAvoidingView behavior="padding">
+              {props.errors.nome && <Error>{props.errors.nome}</Error>}
+              {props.errors.matricula && (
+                <Error>{props.errors.matricula}</Error>
+              )}
+              {props.errors.lotacao && <Error>{props.errors.lotacao}</Error>}
+              {props.errors.cargo && <Error>{props.errors.cargo}</Error>}
+              {props.errors.celular && <Error>{props.errors.celular}</Error>}
+              {props.errors.telefone && <Error>{props.errors.telefone}</Error>}
+              {props.errors.email && <Error>{props.errors.email}</Error>}
 
-        <RowWrapper>
-          <Input title="Genero" icon="wc" />
-          <DatePicker placeholder="Nascimento" />
-        </RowWrapper>
+              <Input
+                onChangeText={props.handleChange('matricula')}
+                onBlur={props.handleBlur('matricula')}
+                value={props.values.matricula}
+                placeholder="matricula"
+                icon="list"
+              />
+              <Input
+                onChangeText={props.handleChange('nome')}
+                onBlur={props.handleBlur('nome')}
+                value={props.values.nome}
+                placeholder="nome"
+                icon="person-pin"
+              />
 
-        <RowWrapper>
-          <Input title="Município de nascimento" icon="call" />
-          <Input title="Matrícula" icon="call" />
-        </RowWrapper>
+              <RowWrapper>
+                <DatePicker placeholder="Nascimento" />
+                <Picker
+                  selectedValue={props.values.sexo}
+                  onValueChange={(itemValue, itemIndex) =>
+                    props.setFieldValue('genero', itemValue)
+                  }>
+                  <Picker.Item label="Masculino" value="M" />
+                  <Picker.Item label="Feminino" value="F" />
+                </Picker>
+              </RowWrapper>
 
-        <RowWrapper>
-          <Picker icon="pan-tool">
-            <Picker.Item label="Solteiro" value="M" />
-            <Picker.Item label="Casado" value="F" />
-            <Picker.Item label="Divorciado" value="F" />
-          </Picker>
-          <Picker icon="swap-calls">
-            <Picker.Item label="Masculino" value="M" />
-            <Picker.Item label="Feminino" value="F" />
-          </Picker>
-        </RowWrapper>
+              <Input
+                onChangeText={props.handleChange('municipioNascimento')}
+                onBlur={props.handleBlur('municipioNascimento')}
+                value={props.values.municipioNascimento}
+                placeholder="Municipio Nascimento"
+                icon="child-friendly"
+              />
 
-        <RowWrapper>
-          <Input title="Cargo" icon="work" />
-          <Input title="Lotação" icon="directions-bus" />
-        </RowWrapper>
+              <Input
+                onChangeText={props.handleChange('estadoNascimento')}
+                onBlur={props.handleBlur('estadoNascimento')}
+                value={props.values.estadoNascimento}
+                placeholder="Estado Nascimento"
+                icon="location-city"
+              />
 
-        <ButtonWrapper>
-          <LinearButton title="SALVAR" onPress={handleClick} />
-        </ButtonWrapper>
-      </Wrapper>
+              <RowWrapper>
+                <Picker
+                  onValueChange={(itemValue, itemIndex) =>
+                    props.setFieldValue('estadoCivil', itemValue)
+                  }
+                  icon="wc">
+                  <Picker.Item label="Solteiro" value="Solteiro" />
+                  <Picker.Item label="Casado" value="Casado" />
+                  <Picker.Item label="Divorciado" value="Divorciado" />
+                </Picker>
+              </RowWrapper>
+
+              <Input
+                onChangeText={props.handleChange('cargo')}
+                onBlur={props.handleBlur('cargo')}
+                value={props.values.cargo}
+                placeholder="Cargo"
+                icon="work"
+              />
+              <Input
+                onChangeText={props.handleChange('lotacao')}
+                onBlur={props.handleBlur('lotacao')}
+                value={props.values.lotacao}
+                placeholder="Lotação"
+                icon="directions-bus"
+              />
+
+              <Separator />
+
+              <RowWrapper>
+                <Input
+                  onChangeText={props.handleChange('celular')}
+                  onBlur={props.handleBlur('celular')}
+                  value={props.values.celular}
+                  placeholder="celular"
+                  icon="smartphone"
+                />
+
+                <Input
+                  onChangeText={props.handleChange('telefone')}
+                  onBlur={props.handleBlur('telefone')}
+                  value={props.values.telefone}
+                  placeholder="Telefone"
+                  icon="local-phone"
+                />
+              </RowWrapper>
+              <Input
+                onChangeText={props.handleChange('email')}
+                onBlur={props.handleBlur('email')}
+                value={props.values.email}
+                placeholder="E-mail"
+                icon="mail"
+              />
+              <ButtonContainer>
+                <LinearButton
+                  type="button"
+                  onPress={props.handleSubmit}
+                  title="SALVAR"
+                />
+              </ButtonContainer>
+            </KeyboardAvoidingView>
+          </Wrapper>
+        )}
+      </Formik>
     </>
   );
 }
 
-PersonEdit.navigationOptions = {
-  title: 'Cadastrar Pessoa',
+PersonCreate.navigationOptions = {
+  title: 'Dados Pessoais',
 };
--- src/pages/Person/Create/styles.js
+++ src/pages/Person/Create/styles.js
@@ -1,26 +1,40 @@
 import styled from 'styled-components/native';
-import {Card} from 'native-base';
 
 export const Container = styled.View`
   flex: 1;
   background: #eee;
 `;
 
-export const Wrapper = styled(Card)`
+export const Wrapper = styled.ScrollView`
   position: absolute;
   flex-direction: column;
   background: #fff;
-  margin: 5px 15px 0;
+  margin: 20px 15px 0;
   padding: 10px 10px 0;
   border-radius: 4px;
-  height: 380;
+  height: 540;
   width: 380;
-`;
-
-export const ButtonWrapper = styled.View`
-  margin-top: 10;
+  align-self: center;
 `;
 
 export const RowWrapper = styled.View`
   flex-direction: row;
+`;
+
+export const Error = styled.Text`
+  width: 360px;
+  font-size: 14;
+  font-weight: bold;
+  color: #fff;
+  background: #d1243e;
+  margin: 4px;
+  padding: 4px;
+  min-height: 15;
+  border-radius: 3;
+  align-self: center;
+  text-align: center;
+`;
+
+export const ButtonContainer = styled.View`
+  margin-bottom: 15px;
 `;
